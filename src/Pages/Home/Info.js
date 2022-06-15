35da679cb8a5f6a5d755c425916829029170c97a
AzmiHabib <azmihabib0323@gmail.com>
2022-06-15 23:57:11 +0600

    fbd

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,7 +6,7 @@ import phone from "../../assets/icons/phone.svg";
 
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 my-12">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 my-11">
       <InfoCard
         cardTitle="Opening Hours"
         cardBody="We are open five days on an week. Please visit us."
