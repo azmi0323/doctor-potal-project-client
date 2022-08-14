9e4952276592c4d35b5162f4e77ffab4e82ed941
AzmiHabib <azmihabib0323@gmail.com>
2022-08-14 21:48:16 +0600

    commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,7 +12,7 @@ import Treatment from './Treatment';
 const Home = () => {
     return (
        <div>
-            <div className='px-12'>
+            <div >
             <Banner></Banner>
             <Info></Info>
             <Services></Services>
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -7,13 +7,13 @@ const MakeAppointment = () => {
     return (
         <section style={{
             background:`url(${appointment})`
-        }} className='flex mt-24 mb-12  justify-center items-center'>
+        }} className='flex mt-24 mb-12  justify-center items-center px-5'>
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-200px]' src={doctor} alt="" />
             </div>
             <div className='flex-1 pb-12'>
                 <h3 className='text-xl text-primary py-6 font-bold'>Appointment</h3>
-                <h2 className='text-3xl text-white font-bold'>Make an Appointment Today</h2>
+                <h2 className='text-3xl text-white font-bold py-5'>Make an Appointment Today</h2>
                 <p className='text-white py-6'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                 <PrimaryButton></PrimaryButton>
             </div>
